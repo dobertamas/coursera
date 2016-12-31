31932e258e4f571b2b34dd1d2f4baad6169f87f8
Tamas Dober <tamas.dober@mobileforming.com>
2016-12-30 19:29:21 -0800

    Added filtering

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,7 +4,8 @@
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .directive('foundItemsDirective', FoundItemsDirective);
+        .directive('foundItemsDirective', FoundItemsDirective)
+        .constant('ApiBasePath', " https://davids-restaurant.herokuapp.com");
 
     function FoundItemsDirective() {
         var ddo = {
@@ -21,42 +22,72 @@
 
     function NarrowItDownController(MenuSearchService) {
         var ctrl = this;
-
-        //var ctrl.status="true";
-
-        var searchTerm = "chicken";
+        ctrl.items=[];
+        var searchTerm = "";
 
         var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
 
         promise.then(function(response) {
-                ctrl.found = response.data;
-                console.log(ctrl.found);
+                ctrl.items = MenuSearchService.found;
+                console.log(ctrl.items);
             })
             .catch(function(error) {
                 console.log("Something went terribly wrong.");
             });
 
         ctrl.onRemove = function(index) {
-            ctrl.found.splice(index, 1);
+            MenuSearchService.remove(index);
         };
     }
 
-    MenuSearchService.$inject = ['$http'];
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 
-    function MenuSearchService($http) {
+    function MenuSearchService($http, ApiBasePath) {
         var service = this;
+        service.found = [];
 
         service.getMatchedMenuItems = function(searchTerm) {
+            service.found = [];
             var response = $http({
-                method: "GET",
-                url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-            });
+                method: 'GET',
+                url: (ApiBasePath + '/menu_items.json')
+            }).then(
 
-            console.log(response);
+                function(response) {
+                    response.data.menu_items.map(
+
+                        function(element) {
+                          console.log(element);
+                            if (element.description.indexOf(searchTerm) != -1 &&
+                                searchTerm != "") {
+                                service.found.push(element);
+                            }
+                        }
+                    )
+                },
+                function(error) {
+                    console.log('error');
+                });
+                //console.log(response);
             return response;
+        }
+        service.remove = function(index) {
+            service.found.splice(index, 1);
+        }
 
-        };
-
+        //
+        //     service.getMatchedMenuItems = function(searchTerm) {
+        //         var response = $http({
+        //             method: "GET",
+        //             url: (ApiBasePath + '/menu_items.json')
+        //         });
+        //
+        //         console.log(response);
+        //         return response;
+        //
+        //     };
+        //
+        // }
     }
 
 })();
